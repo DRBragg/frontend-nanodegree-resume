dab8ea7787dda798f8e3551c17cb4ca6392462ca
Drew Bragg <DRBragg@gmail.com>
2017-07-19 21:47:33 -0400

    Added info objects, per instructions

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,3 +1,30 @@
-/*
-This is empty on purpose! Your code to build the resume will go here.
- */
+let bio = {
+	name: "Drew Bragg",
+	role: "Junior Web Developer",
+	contactInfo: {
+		phone:"267-555-1234",
+		email: "DRBragg@gmail.com"
+	},
+	bioPic: "images/mg.jpg",
+	welcomeMessage: "Hello! And welcome to my online JS resume.",
+	skills: [
+	"HTML", "CSS", "JavaScript"
+	],
+	locaton: "Warrington, PA"
+};
+
+let work = {
+	position: "Web Development Consultant",
+	employer: "Rezzio",
+	years: 1
+};
+
+let education= {
+	name:'Udacity',
+	course: 'Front-End Nanodegree',
+	years: '2017-2018'
+};	
+
+let projects = {
+	title: "Online JS Resume"
+}
